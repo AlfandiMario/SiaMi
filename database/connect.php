add9b6443f6328c74edd4e12bb69646f49ca7921
AlfandiMario <marioalfandi@student.uns.ac.id>
2022-11-14 14:12:24 +0700

    hapus comment

--- database/connect.php
+++ database/connect.php
@@ -3,9 +3,6 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $db = "siportal_kel3";
-//aaaaaaa
+
 // Create connection
 $conn = new mysqli($servername, $username, $password,$db);
-
-
-?>  
